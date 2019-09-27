22e92a74ecc23ad0a21c940036db96d181950368
Kiat Stanley <kiatstanley@usahakreatif.co.id>
2019-09-27 14:55:34 +0700

    Included LuminanceSource to BarcodeResult

--- zxing-android-embedded/src/com/journeyapps/barcodescanner/BarcodeResult.java
+++ zxing-android-embedded/src/com/journeyapps/barcodescanner/BarcodeResult.java
@@ -26,6 +26,8 @@ public class BarcodeResult {
     private static final float PREVIEW_LINE_WIDTH = 4.0f;
     private static final float PREVIEW_DOT_WIDTH = 10.0f;
 
+    public LuminanceSource source;
+
     protected Result mResult;
     protected SourceData sourceData;
 
